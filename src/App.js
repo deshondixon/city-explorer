b6c51a7f4986b56f9ad491280136fb7ff6cd0d71
deshondixon <deshondixon@gmail.com>
2022-11-13 12:49:18 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: ''
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.city.value);
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      city: e.target.value
+
+      });
+    };
+
+  render() {
+    console.log(this.state.city);
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Search for a City
+            <input type="text" name="city" onChange={this.handleInputChange}/>
+          </label>
+          <button type="submit">Search for a City</button>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App;
