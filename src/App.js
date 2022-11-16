8d68ef75aee1910ec7d79fa7df6b78d782592f30
deshondixon <deshondixon@gmail.com>
2022-11-15 20:24:37 -0800

    map appear

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import Badge from 'react-bootstrap/Badge';
-import './App.css';
-
+import React from "react";
+import axios from "axios";
+import Badge from "react-bootstrap/Badge";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: '',
+      city: "",
       cityData: {},
+      errorMessage: "",
       isError: false,
-      errorMessage: '',
-    }
+    };
   }
 
   handleInputChange = (event) => {
     this.setState({
       city: event.target.value,
-    })
-  }
+    });
+  };
 
   handleSubmit = async (event) => {
     try {
       event.preventDefault();
-      let locationInfo = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+      let locationInfo = await axios.get(
+        `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+      );
       this.setState({
         cityData: locationInfo.data[0],
         isError: false,
@@ -33,35 +34,49 @@ class App extends React.Component {
       this.setState({
         errorMessage: error.message,
         isError: true,
-      })
+      });
     }
-  }
+  };
 
   render() {
-
-       //let mapUrl = axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=15`);
+    let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=30`;
 
     return (
       <>
-      <header>
-
-        <h1><Badge>City Explorer</Badge>
-
-        </h1>
-        </header>
+        <header>
+          <h1>
+            {" "}
+            <Badge> City Explorer </Badge>
+          </h1>
+        </header>{" "}
         <main>
-        <form onSubmit={this.handleSubmit}>
-          {this.state.isError ? <p>{this.state.errorMessage}</p> : <ul></ul>}
-          <label>
-            Search for a City
-            <input type="text" name="city" onChange={this.handleInputChange} />
-          </label>
-          <button type="submit">Search for a City</button>
-        </form>
-        </main>
-        <footer>Ⓒ DeShon Dixon 2022</footer>
+          <form onSubmit={this.handleSubmit}>
+            <h2> {this.state.cityData.display_name} </h2>{" "}
+            <img
+              className="map"
+              src={mapURL}
+              alt={this.state.city + "map"}
+            ></img>{" "}
+            <p className="latitudes"> Latitude: {this.state.cityData.lat} </p>{" "}
+            <p className="longitudes"> Latitude: {this.state.cityData.lat} </p>{" "}
+            {this.state.isError ? (
+              <h3> {this.state.errorMessage} </h3>
+            ) : (
+              <ul></ul>
+            )}{" "}
+            <label>
+              <input
+                type="text"
+                name="city"
+                onChange={this.handleInputChange}
+              />{" "}
+            </label>{" "}
+            <button type="submit"> Search for a City </button>{" "}
+          </form>{" "}
+        </main>{" "}
+        <footer> ⒸDeShon Dixon 2022 </footer>{" "}
       </>
-    )
+    );
   }
 }
 
